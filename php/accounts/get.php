0bed66ab56ad387a1b48297321684c20ce039be5
donriga <donriga@yandex.ru>
2014-06-03 18:54:01 +0600

    accounts get list

---
+++ php/accounts/get.php
@@ -0,0 +1,27 @@
+<?php
+
+	//ѕример получени€ списка организаций
+
+	$url = 'https://wirecrm.com/api/accounts';
+	$apikey = 'paste api key';
+
+	$headers = array(
+		"Content-Type: application/json",
+		"X-API-KEY: " . $apikey
+	);
+
+	$handle = curl_init(); 
+	curl_setopt($handle, CURLOPT_URL, $url);
+	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+	$data = curl_exec($handle);
+	curl_close($handle);
+
+	$array = json_decode($data);
+
+	echo '<pre>';
+	print_r($array);
+
+?>
